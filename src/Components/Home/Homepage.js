7425a33c017a9d5e39fc9430fe8039c92835c5a3
Sahil Singh <sahils091@gmail.com>
2020-12-31 15:47:59 -0500

    Set up validation and conditional rendering for the email field

--- src/Components/Home/Homepage.js
+++ src/Components/Home/Homepage.js
@@ -1,41 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./homepage.scss";
 
 const Homepage = () => {
+  const [error, setError] = useState(false);
+  const [submit, setSubmit] = useState(false);
+  const [loading, setLoading] = useState(undefined);
+  const loadingFunc = (e)=>{
+    setLoading(false);
+    console.log("Does this work??")
+    setSubmit(true);
+    e.target.reset();
+    console.log(submit)
+  }
+  const submitHandler = (e) =>{
+   e.preventDefault();
+   setTimeout(loadingFunc, 2000);
+   setLoading(true)
+   console.log(loading , "loading is ??");
+    console.log(error);
+   let emailValue = e.target.email.value;
+   let interestValue = e.target.interest.value;
+   emailValue.includes('@') ? setError(false) : setError(true);
+   
+   console.log(`Email is "${emailValue}"`);
+   console.log(`Area of Interest is "${interestValue}"`);
+   
+     
+ 
+  }
   return (
     <main className="home">
       <section className="home__banner">
-        <div className="home__banner-wrapper">
+        <div className="home__banner-wrapper">  
           <h1 className="home__banner-title">INTERNSHIP SIGNUP FORM</h1>
 
           <div className="home__banner-border"></div>
 
-          <p className="home__banner-blurb">
+          <p className="home__banner-blurb" >
             Prepare for your career with a Project Management, Web-Development,
             Graphic Design, or Digital Marketing Internship at Northern.
           </p>
         </div>
       </section>
       <section className="home__input">
-        <form className="home__input-form">
-        <div className=" home__input-error--wrapper">
-        <p className=" home__input-error--blurb">
+        <form className="home__input-form" onSubmit={submitHandler}>
+          <div className=" home__input-error--wrapper">
+            <p className= {!error ? "home__input-error--blurb" : "home__input-error--conditional" }>
               Please enter a valid email address.
-          </p>  
-        </div>
-        
+            </p>
+          </div>
+
           <div className="home__input-wrapper">
-         
             <input
-              className="home__input-email"
+            className= {!error ? "home__input-email" : "home__input-email--conditional" }
               name="email"
               placeholder="Your Email Address * "
               type="email"
-              required={true}
             />
             <select
               className="home__input-interest"
               placeholder="Your Interests"
+              name="interest"
             >
               <option value="" className="home__input-interest-option">
                 Your Interests
@@ -68,9 +93,13 @@ const Homepage = () => {
               </option>
             </select>
           </div>
-          <button className="home__input-btn">
-            Sign Up Now{" "}
-            <i className="fa fa-caret-right home__input-btn--icon"></i>
+          {/* disabled={} */}
+          <button className="home__input-btn" > 
+          {
+            loading ? "Submitting..." : "Sign Up Now" 
+            }
+            
+            <i className= { loading ? "home__btn--conditional" : "fa fa-caret-right home__input-btn--icon"}></i>
           </button>
         </form>
       </section>
